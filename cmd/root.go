7cb37152b0a9fb5a28863a7192ce027da8845b04
Marco Bravo Mejías <marco@layfellow.net>
2022-10-10 23:06:32 -0300

    Read configuration file

--- cmd/root.go
+++ cmd/root.go
@@ -2,18 +2,20 @@ package cmd
 
 import (
 	"fmt"
+	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 	"os"
 )
 
+var cfgFile string
+
 var rootCmd = &cobra.Command{
-	Use:   "hugo",
-	Short: "Hugo is a very fast static site generator",
-	Long: `A Fast and Flexible Static Site Generator built with
-                love by spf13 and friends in Go.
-                Complete documentation is available at http://hugo.spf13.com`,
+	Use:   "todo",
+	Short: "Minimal todoist CLI client",
+	Long:  "Lorem ipsum dolor sit amet", // TODO
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("hugo")
+		fmt.Println("Usage: todo")
 	},
 }
 
@@ -21,5 +23,32 @@ func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo.yaml)")
+}
+
+func initConfig() {
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		viper.AddConfigPath(home)
+		viper.SetConfigType("yaml")
+		viper.SetConfigName(".todo")
+	}
+
+	viper.AutomaticEnv()
+
+	if err := viper.ReadInConfig(); err == nil {
+		// fmt.Println("Config file:", viper.ConfigFileUsed())
 	}
 }
