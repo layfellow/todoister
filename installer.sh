30c46650d929ff4a0eecffd5da8ea57f4161f639
Marco Bravo Mejías <marco@layfellow.net>
2024-11-20 17:13:34 -0300

    Add installer script

---
+++ installer.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+set -eu
+
+base_url="https://github.com/layfellow/todoister/releases/latest/download"
+binary="todoister"
+
+main() {
+	os=$( uname -s )
+	arch=$( uname -m )
+
+	case "$os-$arch" in
+		Darwin-arm64)
+			url="$base_url/$binary-darwin-arm64"
+			;;
+		Darwin-x86_64)
+			url="$base_url/$binary-darwin-amd64"
+			;;
+		Linux-x86_64)
+			url="$base_url/$binary-linux-amd64"
+			;;
+		*)
+			printf "installer: sorry, native Windows is not supported.\n" >&2
+			exit 1
+			;;
+	esac
+
+	if command -v curl >/dev/null 2>&1; then
+		fetch_cmd="curl -sSL -o $binary \"$url\""
+	elif command -v wget >/dev/null 2>&1; then
+		fetch_cmd="wget -qO $binary \"$url\""
+	else
+		printf "installer: neither curl nor wget found; please install one of them.\n" >&2
+		exit 1
+	fi
+
+	eval "$fetch_cmd"
+
+	chmod +x $binary
+
+	if [ -d "$HOME/.local/bin" ]; then
+		mv $binary "$HOME/.local/bin"
+	elif [ -d "$HOME/bin" ]; then
+		mv $binary "$HOME/bin"
+	else
+		printf "installer: neither \$HOME/.local/bin nor \$HOME/bin found; leaving binary in current directory.\n" >&2
+	fi
+
+	printf "installer: %s installed successfully.\n" "$binary"
+}
+
+main
